0262b6d943b21020b1e8048ef3caad913834be5b
Li-Aaron <ac2sherry@126.com>
2019-03-07 16:37:02 +0800

    TestPkg/TestPcdApp: An Application to test all kinds of PCD (Updated).
    update missing files.

---
+++ TestPkg/Include/Pcd/TestPcd.h
@@ -0,0 +1,28 @@
+/** @file
+  Provides Pcd Type Structures.
+
+  Self study usage.
+**/
+
+#ifndef __TEST_PCD_H__
+#define __TEST_PCD_H__
+
+// #include <Uefi.h>
+
+#pragma pack(1)
+typedef struct {
+  BOOLEAN  Bool;  
+  UINT8    A:4; 
+  UINT8    B:4;
+  UINT16   C;
+  UINT16   D;
+  union {
+    UINT8   Foo[8];
+    UINT64  Bar;
+  } E;
+  UINT8    Guid[16];
+  UINT8    FlexibleArray[];
+} TESTPCD;
+#pragma pack()
+
+#endif
