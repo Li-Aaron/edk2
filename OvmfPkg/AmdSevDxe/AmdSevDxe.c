65f794b68f54dc7845284ecb806873e44918f768
Aaron Li <ac2sherry@gmail.com>
2021-07-26 16:06:24 +0800

    Revert "OvmfPkg/AmdSevDxe: use MemEncryptSevClearMmioPageEncMask() to clear EncMask"
    This reverts commit 60285fb8b4efb341dc9b07b419d7aab4e62b5aad.

--- OvmfPkg/AmdSevDxe/AmdSevDxe.c
+++ OvmfPkg/AmdSevDxe/AmdSevDxe.c
@@ -53,10 +53,11 @@ AmdSevDxeEntryPoint (
       Desc = &AllDescMap[Index];
       if (Desc->GcdMemoryType == EfiGcdMemoryTypeMemoryMappedIo ||
           Desc->GcdMemoryType == EfiGcdMemoryTypeNonExistent) {
-        Status = MemEncryptSevClearMmioPageEncMask (
+        Status = MemEncryptSevClearPageEncMask (
                    0,
                    Desc->BaseAddress,
-                   EFI_SIZE_TO_PAGES (Desc->Length)
+                   EFI_SIZE_TO_PAGES (Desc->Length),
+                   FALSE
                    );
         ASSERT_EFI_ERROR (Status);
       }
@@ -72,10 +73,11 @@ AmdSevDxeEntryPoint (
   // the range.
   //
   if (PcdGet16 (PcdOvmfHostBridgePciDevId) == INTEL_Q35_MCH_DEVICE_ID) {
-    Status = MemEncryptSevClearMmioPageEncMask (
+    Status = MemEncryptSevClearPageEncMask (
                0,
                FixedPcdGet64 (PcdPciExpressBaseAddress),
-               EFI_SIZE_TO_PAGES (SIZE_256MB)
+               EFI_SIZE_TO_PAGES (SIZE_256MB),
+               FALSE
                );
 
     ASSERT_EFI_ERROR (Status);
