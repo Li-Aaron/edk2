7b15806d6944eabea0d683cbbdcf490ffa7dfceb
Aaron Li <ac2sherry@gmail.com>
2021-07-26 16:06:24 +0800

    Revert "OvmfPkg/PciHostBridgeLibScan: clean up file names and file-top comments"
    This reverts commit 483eb3b2b26ce5f5ae37f0ac127f19c08e257773.

--- OvmfPkg/Library/PciHostBridgeLibScan/PciHostBridge.h
+++ OvmfPkg/Library/PciHostBridgeLibScan/PciHostBridge.h
@@ -1,6 +1,5 @@
 /** @file
-  OVMF's instance of the PCI Host Bridge Library, for Bhyve and Xen guests;
-  internal declarations.
+  Header file of OVMF instance of PciHostBridgeLib.
 
   Copyright (C) 2021, Red Hat, Inc.
   Copyright (c) 2016, Intel Corporation. All rights reserved.<BR>
