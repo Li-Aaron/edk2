73ec024b733e16e387942f9386f3a068e3d6655b
Aaron Li <ac2sherry@gmail.com>
2025-02-06 22:35:18 +0800

    TestAcpi: OpRegion https://li-aaron.github.io/2023/04/asl-code-intro4/
    Signed-off-by: Aaron Li <ac2sherry@gmail.com>

--- OvmfPkg/TestAcpiDxe/TestAcpi.asl
+++ OvmfPkg/TestAcpiDxe/TestAcpi.asl
@@ -22,6 +22,44 @@ DefinitionBlock (
       {
         Return (0x0f)
       }
+
+      // https://li-aaron.github.io/2023/04/asl-code-intro4/
+      OperationRegion (TEST, SystemMemory, 0xA5A5A5A5, 0xA5A5A5A5)
+      Field (TEST, ByteAcc, NoLock, Preserve) {
+        ITER, 32,  // Iteration
+        //
+        // 16 bit x 32 = 512 bits.
+        //
+        IBUF, 512, // Buffer
+      }
+      Method(EXEC, 0, Serialized) {
+        Name (BUFF, Buffer(64){0}) //64 bytes = 512 bits.
+        // BUFF[0] = 1
+        // BUFF[1] = 1
+        Store (0x1, Index(BUFF, 0))
+        Store (0x1, Index(BUFF, 2)) //Buffer index is byte, IBUF index is word.
+        Store (2, Local0)
+        // BUFF[i] = BUFF[i-2] + BUFF[i-1]
+        // i in [2, ITER) -- Local0
+        While (LLess(Local0, ITER)) {
+          Multiply (Local0, 2, Local1) // 1 Word = 2 Byte
+          Store (DerefOf(Index(BUFF, Subtract(Local1, 3))),Local2)        // BUFF[i-2] higher byte
+          ShiftLeft (Local2, 8, Local2)
+          Add (Local2, DerefOf(Index(BUFF, Subtract(Local1, 4))), Local2) // BUFF[i-2] lower byte
+
+          Store (DerefOf(Index(BUFF, Subtract(Local1, 1))),Local3)        // BUFF[i-1] higher byte
+          ShiftLeft (Local3, 8, Local3)
+          Add (Local3, DerefOf(Index(BUFF, Subtract(Local1, 2))), Local3) // BUFF[i-1] lower byte
+
+          Add (Local2, Local3, Local4)
+
+          Store (And(Local4, 0xFF), Index(BUFF, Local1))                  // BUFF[i] lower byte
+          ShiftRight(Local4, 8, Local4)
+          Store (And(Local4, 0xFF), Index(BUFF, Add(Local1, 1)))          // BUFF[i] higher byte
+          Increment (Local0)
+        }
+        Store (BUFF, IBUF)
+      }
     }
   }
 }
--- OvmfPkg/TestAcpiDxe/TestAcpi.c
+++ OvmfPkg/TestAcpiDxe/TestAcpi.c
@@ -7,10 +7,83 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/UefiLib.h>
 #include <Library/DebugLib.h>
+#include <Library/BaseMemoryLib.h>
 #include <Library/MemoryAllocationLib.h>
 #include <IndustryStandard/Acpi.h>
 #include <Protocol/AcpiTable.h>
 #include <Library/DxeServicesLib.h>
+
+// https://li-aaron.github.io/2023/04/asl-code-intro4/
+// DefOpRegion := OpRegionOp NameString RegionSpace RegionOffset RegionLen
+// OpRegionOp := ExtOpPrefix 0x80
+// ExtOpPrefix := 0x5B
+// RegionSpace := ByteData  (0x00 SystemMemory)
+// RegionOffset := TermArg => Integer (0x0A)
+// RegionLen := TermArg => Integer
+// TermArg := ExpressionOpcode | [DataObject] | ArgObj | LocalObj
+// DataObject := [ComputationalData] | DefPackage | DefVarPackage
+// ComputationalData := ByteConst | WordConst | [DWordConst] | QWordConst | String | ConstObj | RevisionOp | DefBuffer
+// DWordConst := DWordPrefix DWordData
+// DWordPrefix := 0x0C
+#pragma pack(push, 1)
+typedef struct {
+  UINT8     ExtOpPrefix;
+  UINT8     ExtOpCode;
+  UINT32    NameString;
+  UINT8     RegionSpace;
+  UINT8     DwordPrefix1;
+  UINT32    RegionOffset;
+  UINT8     DwordPrefix2;
+  UINT32    RegionLen;
+} TEST_OP_REGION;
+
+typedef struct {
+  UINT32                Iteration;
+  UINT16                Buffer[0x20];
+} ACPI_TEST_BUFFER;
+#pragma pack(pop)
+
+ACPI_TEST_BUFFER   *mAcpiTestBuffer;
+
+EFI_STATUS
+EFIAPI
+PatchSsdtTable (
+  IN OUT EFI_ACPI_DESCRIPTION_HEADER  *Table
+  )
+{
+  EFI_STATUS Status;
+  TEST_OP_REGION *OpRegion;
+  UINT8 *Start;
+  UINT8 *End;
+  UINT8 *BytePtr;
+
+  Start = (UINT8 *)(Table + 1);
+  End = Start + Table->Length;
+  Status = EFI_NOT_FOUND;
+
+  // Find the OpRegion in the SSDT table
+  for (BytePtr = Start; BytePtr < End; BytePtr++) {
+    OpRegion = (TEST_OP_REGION *)BytePtr;
+
+    // 5B 80 54 45 53 54 00 0C A5 A5 A5 A5 0C A5 A5 A5 A5
+    if (OpRegion->ExtOpPrefix == 0x5B
+        && OpRegion->ExtOpCode == 0x80
+        && OpRegion->NameString == SIGNATURE_32('T', 'E', 'S', 'T')
+        && OpRegion->RegionSpace == 0x00
+        && OpRegion->DwordPrefix1 == 0x0C
+        && OpRegion->DwordPrefix2 == 0x0C
+        ) {
+      // Patch the OpRegion
+      OpRegion->RegionOffset = (UINT32) (UINTN) mAcpiTestBuffer;
+      OpRegion->RegionLen = (UINT32) (UINTN) sizeof(ACPI_TEST_BUFFER);
+      Status = EFI_SUCCESS;
+      break;
+    }
+  }
+
+  return Status;
+}
+
 
 EFI_STATUS
 EFIAPI
@@ -36,6 +109,28 @@ InstallSsdtTable (
   Status = GetSectionFromFv(&gEfiCallerIdGuid, EFI_SECTION_RAW, 0, (VOID **)&SsdtTable, &SsdtTableSize);
   if (EFI_ERROR(Status)) {
     DEBUG((EFI_D_ERROR, "%a, Failed to get SSDT table from firmware volume: %r\n", __func__, Status));
+    return Status;
+  }
+
+  // Allocate memory for the ACPI test buffer
+  Status = gBS->AllocatePool (
+                  EfiRuntimeServicesData,
+                  sizeof (ACPI_TEST_BUFFER),
+                  (VOID **) &mAcpiTestBuffer
+                  );
+  if (EFI_ERROR (Status)) {
+    DEBUG((EFI_D_ERROR, "%a, Failed to allocate memory for ACPI test buffer: %r\n", __func__, Status));
+    FreePool(SsdtTable);
+    return Status;
+  }
+  ZeroMem (mAcpiTestBuffer, sizeof (ACPI_TEST_BUFFER));
+  mAcpiTestBuffer->Iteration = 0x18;
+
+  // Patch SSDT table's OpRegion
+  Status = PatchSsdtTable(SsdtTable);
+  if (EFI_ERROR(Status)) {
+    DEBUG((EFI_D_ERROR, "%a, Failed to patch SSDT table: %r\n", __func__, Status));
+    FreePool(SsdtTable);
     return Status;
   }
 
